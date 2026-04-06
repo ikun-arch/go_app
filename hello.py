b80c3fe2b6738794bea2d9ce1780e8f420c35c6d
ikun-arch <2725238260@qq.com>
2026-04-06 15:13:07 +0800

    feat:add hello world functio

---
+++ hello.py
@@ -0,0 +1,11 @@
+# hello.py
+def say_hello():
+    print("Hello from PR!")
+
+if __name__ == "__main__":
+    say_hello()# hello.py
+def say_hello():
+    print("Hello from PR!")
+
+if __name__ == "__main__":
+    say_hello()
